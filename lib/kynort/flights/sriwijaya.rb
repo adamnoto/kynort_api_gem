3f4c5f760f1359e0efff523d329fd4cf490ef0b9
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-08-23 08:04:43 +0700

    kynort

--- lib/kynort/flights/sriwijaya.rb
+++ lib/kynort/flights/sriwijaya.rb
@@ -3,7 +3,8 @@ class Kynort::Flights::Sriwijaya
     def search(query)
       raise "query must be an instance of Kynort::Flights::Query" unless query.is_a?(Kynort::Flights::Query)
       query_hash = query.to_hash
-      reply = RestClient.post "http://localhost:4001/api/v1/flights/search/sriwijaya.json", query_hash
+      reply = RestClient.post "http://localhost:4001/api/v1/flights/search/sriwijaya.json", params: query_hash, accept: json
+      reply
     end
 
     def pick(query)
