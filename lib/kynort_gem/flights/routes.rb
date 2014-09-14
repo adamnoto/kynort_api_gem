875ff8de523fe2647405630ceb3947562d04e6ed
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-09-15 06:12:12 +0700

    routes by country

--- lib/kynort_gem/flights/routes.rb
+++ lib/kynort_gem/flights/routes.rb
@@ -3,7 +3,7 @@ module Kynort::Flights::Routes
 
   def suggest_routes(origin)
     params = {access_token: Kynort.token}
-    params[:origin] = origin if origin
+    params[:origin] = origin
 
     response = RestClient.get "http://localhost:4001/api/v1/flights/routes", params: params
     response = JSON.parse(response).with_indifferent_access
@@ -11,6 +11,11 @@ module Kynort::Flights::Routes
   end
 
   def suggest_routes_by_country(origin)
+    params = {access_token: Kynort.token}
+    params[:origin] = origin
 
+    response = RestClient.get "#{Kynort.config.host}/api/v1/flights/routes/by_country", params: params
+    response = JSON.parse(response).with_indifferent_access
+    response[:airports]
   end
 end
