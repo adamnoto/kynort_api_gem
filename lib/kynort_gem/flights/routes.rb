135f6556f122427996e838323d6efa1bdcda8860
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-09-15 06:32:59 +0700

    routes now allow nil origin

--- lib/kynort_gem/flights/routes.rb
+++ lib/kynort_gem/flights/routes.rb
@@ -1,18 +1,18 @@
 module Kynort::Flights::Routes
   module_function
 
-  def suggest_routes(origin)
+  def suggest_routes(origin = nil)
     params = {access_token: Kynort.token}
-    params[:origin] = origin
+    params[:origin] = origin if origin
 
     response = RestClient.get "http://localhost:4001/api/v1/flights/routes", params: params
     response = JSON.parse(response).with_indifferent_access
     response[:airports]
   end
 
-  def suggest_routes_by_country(origin)
+  def suggest_routes_by_country(origin = nil)
     params = {access_token: Kynort.token}
-    params[:origin] = origin
+    params[:origin] = origin if origin
 
     response = RestClient.get "#{Kynort.config.host}/api/v1/flights/routes/by_country", params: params
     response = JSON.parse(response).with_indifferent_access
